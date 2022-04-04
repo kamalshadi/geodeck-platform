dc2826bfd302abcc4446d1b26bd77c116f6f9121
amirdeljouyi <amirdeljouyi@gmail.com>
2022-04-04 16:39:34 +0430

    generate buckets automatically

--- aws/buckets.sh
+++ aws/buckets.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -x
-awslocal s3 mb s3://geodeck-gallery-files
-awslocal s3 mb s3://geodeck-static-files
-awslocal s3 mb s3://geodeck-gltf-files
-awslocal s3 mb s3://geodeck-hdf5-files
+awslocal s3api create-bucket --bucket geodeck-gallery-files --region us-east-1 --acl public-read-write
+awslocal s3api create-bucket --bucket geodeck-static-files --region us-east-1 --acl public-read-write
+awslocal s3api create-bucket --bucket geodeck-gltf-files --region us-east-1 --acl public-read-write
+awslocal s3api create-bucket --bucket geodeck-hdf5-files --region us-east-1 --acl public-read-write
+awslocal s3api put-object --bucket geodeck-static-files --key default.png --body /docker-entrypoint-initaws.d/default.png
+awslocal s3api put-bucket-cors --bucket geodeck-hdf5-files --cors-configuration  file:///docker-entrypoint-initaws.d/cors.json
+awslocal s3api put-bucket-cors --bucket geodeck-gltf-files --cors-configuration  file:///docker-entrypoint-initaws.d/cors.json
 set +x
